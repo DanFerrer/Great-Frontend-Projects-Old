7123626e3af78849662bc0b43690aeb78fc3bb9f
DanFerrer <danielferrer212@gmail.com>
2024-07-13 17:20:30 -0400

    add fonts

--- GreatFrontEnd/apps/chat-ai/tailwind.config.js
+++ GreatFrontEnd/apps/chat-ai/tailwind.config.js
@@ -2,8 +2,10 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      'noto-sans': ['Noto Sans', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif'],
+    },
   },
   plugins: [],
-}
+};
 
