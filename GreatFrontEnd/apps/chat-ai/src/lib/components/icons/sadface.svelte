6f7831e5b96a9ad139c41ba8fa5634a072557373
DanFerrer <danielferrer212@gmail.com>
2024-07-13 16:46:54 -0400

    add icons

---
+++ GreatFrontEnd/apps/chat-ai/src/lib/components/icons/sadface.svelte
@@ -0,0 +1,17 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="28" height="30" viewBox="0 0 28 30" fill="none">
+  <g filter="url(#filter0_d_2_521)">
+    <path d="M14 2C19.5228 2 24 6.47715 24 12C24 12.7266 23.9225 13.4351 23.7753 14.1177L21.9931 12.3355C21.9977 12.2242 22 12.1124 22 12C22 7.58172 18.4183 4 14 4C9.58172 4 6 7.58172 6 12C6 16.4183 9.58172 20 14 20C15.3023 20 16.5319 19.6888 17.6186 19.1368C17.7735 19.3828 17.958 19.6149 18.1716 19.8284C18.4739 20.1307 18.8125 20.3745 19.1734 20.5598C17.6642 21.4737 15.8936 22 14 22C8.47715 22 4 17.5228 4 12C4 6.47715 8.47715 2 14 2ZM21 14.1716L22.4142 15.5858C23.1953 16.3668 23.1953 17.6332 22.4142 18.4142C21.6332 19.1953 20.3668 19.1953 19.5858 18.4142C18.8402 17.6687 18.8064 16.481 19.4841 15.6952L19.5858 15.5858L21 14.1716ZM14 15C15.4664 15 16.7853 15.6312 17.6999 16.6368L16.7549 17.4961C15.965 17.1825 15.018 17 14 17C12.982 17 12.035 17.1825 11.2451 17.4961L10.3001 16.6368C11.2147 15.6312 12.5336 15 14 15ZM10.5 10C11.3284 10 12 10.6716 12 11.5C12 12.3284 11.3284 13 10.5 13C9.67157 13 9 12.3284 9 11.5C9 10.6716 9.67157 10 10.5 10ZM17.5 10C18.3284 10 19 10.6716 19 11.5C19 12.3284 18.3284 13 17.5 13C16.6716 13 16 12.3284 16 11.5C16 10.6716 16.6716 10 17.5 10Z" fill="#4338CA"/>
+  </g>
+  <defs>
+    <filter id="filter0_d_2_521" x="-2" y="0" width="32" height="32" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feOffset dy="4"/>
+      <feGaussianBlur stdDeviation="2"/>
+      <feComposite in2="hardAlpha" operator="out"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2_521"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2_521" result="shape"/>
+    </filter>
+  </defs>
+</svg>
